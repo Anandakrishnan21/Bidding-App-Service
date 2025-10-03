91c29d431f7c86b8050dcbeb30ac229e87e3d2c5
Anandakrishnan21 <anandakrishnandinej02@gmail.com>
2025-10-03 22:23:56 +0530

    Initial commit

---
+++ src/test/java/com/personal/bidding_java_service/BiddingJavaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.bidding_java_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiddingJavaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
